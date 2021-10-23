c87cbc8cf4d1cf778faaa2cbbe27648080fe6c7f
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-10-23 15:49:05 +0530

    Stdin and stdout

--- firstweekassignment/src/Basics/JavaStdinandStdout2.java
+++ firstweekassignment/src/Basics/JavaStdinandStdout2.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 
 public class JavaStdinandStdout2 {
 	
-
-
-
-	    @SuppressWarnings("resource")
 		public static void main(String[] args) {
 	        Scanner scan = new Scanner(System.in);
 	        int i = scan.nextInt();
